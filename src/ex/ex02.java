17246dda6124f859462fe5cfb18e0e1de9338ca9
qwa1822 <58835205+qwa1822@users.noreply.github.com>
2022-03-13 16:29:23 +0900

    Initial commit

---
+++ src/ex/ex02.java
@@ -0,0 +1,17 @@
+package ex;
+
+import java.util.Scanner;
+
+public class ex02 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
+        int a,b;
+        a=scanner.nextInt();
+        b=scanner.nextInt();
+
+        System.out.println(a-b);
+    }
+}
---
+++ src/ex/hello.java
@@ -0,0 +1,18 @@
+package ex;
+
+import java.util.Scanner;
+
+public class hello {
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
+                int a,b;
+        a=scanner.nextInt();
+        b=scanner.nextInt();
+
+        System.out.println(a+b);
+
+    }
+}
